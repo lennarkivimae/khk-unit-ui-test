5ae180966ff38a44c02abb7fa79b2f41cd6444b7
lennarkivimae <lennar.kivimae@gmail.com>
2021-05-14 17:13:20 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RestaurantScreen />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import RestaurantScreen from '@/components/RestaurantScreen';
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+      RestaurantScreen
   },
 };
 </script>
